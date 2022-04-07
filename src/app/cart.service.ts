b781ac2b7c7d6f6cb3300f5919ff536b06ea10cc
daiglewilliam13 <daiglewilliam13@gmail.com>
2022-04-07 13:28:35 -0700

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +19,5 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  constructor() {}
+  constructor(private http: HttpClient) {}
 }
